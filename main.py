ffe5dad2bd83c476e611868793555077f04057ea
Shuei YAMADA <shuei.yamada+github@gmail.com>
2022-06-09 00:43:22 +0900

    更新 main.blocks, main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def on_gesture_free_fall():
+    basic.show_icon(IconNames.HEART)
+input.on_gesture(Gesture.FREE_FALL, on_gesture_free_fall)
+
 def on_forever():
-    led.plot(3, 0)
+    led.plot_brightness(4, 0, (input.acceleration(Dimension.X) + 1023) / 8)
+    led.plot_brightness(0, 0, (1023 - input.acceleration(Dimension.X)) / 8)
+    led.plot_brightness(2, 2, 255 - input.light_level())
 basic.forever(on_forever)
