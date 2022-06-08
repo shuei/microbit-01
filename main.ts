171f2865dfe70bc01e2247c3577fb9473fa6e814
Shuei YAMADA <shuei.yamada+github@gmail.com>
2022-06-09 00:29:29 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,5 @@
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . #
-        . . . . .
-        . . . . .
-        . . . . .
-        # . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
+    led.plotBrightness(4, 0, (input.acceleration(Dimension.X) + 1023) / 8)
+    led.plotBrightness(0, 0, (1023 - input.acceleration(Dimension.X)) / 8)
+    led.plotBrightness(2, 2, 255 - input.lightLevel())
 })
