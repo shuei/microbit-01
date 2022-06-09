a4bd69c67c3a9c6df283feacdcc1b2b9dc0ff7ac
Shuei YAMADA <shuei.yamada+github@gmail.com>
2022-06-09 22:08:49 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 basic.forever(function () {
-    led.plotBrightness(4, 0, (input.acceleration(Dimension.X) + 1023) / 8)
-    led.plotBrightness(0, 0, (1023 - input.acceleration(Dimension.X)) / 8)
+    led.plotBrightness(4, 2, (input.acceleration(Dimension.X) + 1023) / 8)
+    led.plotBrightness(0, 2, (1023 - input.acceleration(Dimension.X)) / 8)
+    led.plotBrightness(2, 0, (1023 - input.acceleration(Dimension.Y)) / 8)
+    led.plotBrightness(2, 4, (input.acceleration(Dimension.Y) + 1023) / 8)
     led.plotBrightness(2, 2, 255 - input.lightLevel())
 })
